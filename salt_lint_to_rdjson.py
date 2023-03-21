e814b557b399257f06f74280df24e3bffe2ee98c
Kyle Everling <30862822+lordsherman@users.noreply.github.com>
2023-03-21 00:14:27 -0400

    Try new function

--- salt_lint_to_rdjson.py
+++ salt_lint_to_rdjson.py
@@ -7,21 +7,53 @@ def parse_salt_lint_output(output):
     pattern = r'\[(\d+)\] (.+?)\n(.*?):(\d+)'
     matches = re.finditer(pattern, output, re.MULTILINE)
 
-    diagnostics = []
+    diagnostics = {
+        "source": {
+            "name": "salt-lint",
+            "url": "https://example.com/url/to/salt-lint"
+        },
+        "severity": "ERROR",
+        "diagnostics": []
+    }
+
     for match in matches:
+        rule_number = match.group(1)
+        rule_message = match.group(2)
+        file_path = match.group(3)
+        line_number = int(match.group(4)) - 1
+
+        if rule_number.startswith('2'):
+            rule_url = f"https://salt-lint.readthedocs.io/en/latest/rules/formatting/#{rule_number}"
+        elif rule_number.startswith('8'):
+            rule_url = f"https://salt-lint.readthedocs.io/en/latest/rules/recommendations/#{rule_number}"
+        elif rule_number.startswith('9'):
+            rule_url = f"https://salt-lint.readthedocs.io/en/latest/rules/deprecations/#{rule_number}"
+        else:
+            rule_url = "https://salt-lint.readthedocs.io/en/latest/rules/"
+
         diagnostic = {
-            "source": {"name": "salt-lint"},
-            "severity": "ERROR",
-            "message": f"{match.group(1)}: {match.group(2)}",
+            "message": f"{rule_number}: {rule_message}",
             "location": {
-                "path": match.group(3),
+                "path": file_path,
                 "range": {
-                    "start": {"line": int(match.group(4)) - 1, "column": 0},
-                    "end": {"line": int(match.group(4)) - 1, "column": 0},
-                },
+                    "start": {
+                        "line": line_number,
+                        "column": 0
+                    },
+                    "end": {
+                        "line": line_number,
+                        "column": 0
+                    }
+                }
             },
+            "severity": "ERROR",
+            "code": {
+                "value": rule_number,
+                "url": rule_url
+            }
         }
-        diagnostics.append(diagnostic)
+
+        diagnostics["diagnostics"].append(diagnostic)
 
     return diagnostics
 
